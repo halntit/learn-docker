e457590dd86f43ef596b24c38b41c5acdafc518e
Ha Le <halntit@gmail.com>
2024-03-16 00:53:10 +0700

    standalone fe

--- 13-example-12/frontend/src/App.js
+++ 13-example-12/frontend/src/App.js
@@ -7,7 +7,7 @@ import ErrorAlert from './components/UI/ErrorAlert';
 const backendUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost'
-    : 'http://ecs-lb-783189166.us-east-2.elb.amazonaws.com';
+    : 'http://goals-node-lb-1482169531.ap-southeast-1.elb.amazonaws.com';
 
 function App() {
   const [loadedGoals, setLoadedGoals] = useState([]);
